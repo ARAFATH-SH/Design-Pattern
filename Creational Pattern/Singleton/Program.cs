49cd304c0cdea5666d2a295ddaf2aebec9e9ac1f
Arafath <alarafath.info@gmail.com>
2026-06-19 01:17:47 +0600

    Singleton Pattern

---
+++ Creational Pattern/Singleton/Program.cs
@@ -0,0 +1,36 @@
+﻿class Singleton
+{
+    private static Singleton instance;
+    Singleton()
+    {
+        
+    }
+
+    public static Singleton GetInstance()
+    {
+        if(instance == null)
+        {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var s1 =  Singleton.GetInstance();
+        var s2 = Singleton.GetInstance();
+
+        if(s1 == s2)
+        {
+            Console.WriteLine("Both are same");
+        }
+        else
+        {
+            Console.WriteLine("Both are different");
+        }
+    }
+}
